3102acf2b62eb4e3be5d2a06395ccbb0df304e0d
RowanDurrant <r.durrant.1@research.gla.ac.uk>
2026-03-09 15:27:47 -0400

    document data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example phylogenetic tree
+#'
+#' A phylogenetic tree generated from simulated data.
+#'
+#' @format ## `example_tree`
+#' An object of class phylo with 59 tips.
+"example_tree"
+
+#' Example BEAST log clock rate values
+#'
+#' A vector containing simulated values of the substitution rate.
+#'
+#' @format ## `mock_BEAST_clockrates`
+#' A vector of length 10,000.
+"mock_BEAST_clockrates"
